9857d83a5c674c19ada51ee7cc23b185232a26de
Benjamin Fritsch <ben@lomography.com>
2012-04-23 14:30:39 +0200

    encoding foo

--- minify.rb
+++ minify.rb
@@ -1,5 +1,5 @@
-#!/usr/bin/env ruby
+# encoding: UTF-8
 
-html = File.read("index.maximized.html")
+html = File.read("index.maximized.html").force_encoding("UTF-8")
 minified = html.gsub(/$\s*/, "").gsub("\n", "")
 File.open("index.html", 'w') {|f| f.write(minified) }
